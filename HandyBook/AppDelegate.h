da8dc2f3646fe7e976f54c6d995595362a71f4cc
Sema Belokovsky <grampe@nulana.com>
2013-02-17 03:59:24 +0600

    Initial Commit

---
+++ HandyBook/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HandyBook
+//
+//  Created by Sema Belokovsky on 17.02.13.
+//  Copyright (c) 2013 Sema Belokovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
